284a2151cedf731dca3f74c992599dd7fdb0439b
Raja1919 <rajaaart51@gmail.com>
2023-07-19 21:41:24 +0530

    second commit

--- Routes/urlshortener.js
+++ Routes/urlshortener.js
@@ -1,57 +1,53 @@
 const express = require("express");
 const UrlModel = require("../model/urlshortener");
 const routes = express.Router();
-const shortid=require("shortid")
-
+const shortid = require("shortid");
 
 routes.get("/all", async (req, res) => {
-    const data = await UrlModel.find();
-    if (!data) {
-      return res.status(404).json({ message: "not found" });
-    }
-    res.status(200).json(data);
+  const data = await UrlModel.find();
+  if (!data) {
+    return res.status(404).json({ message: "not found" });
+  }
+  res.status(200).json(data);
+});
+
+routes.post("/create", async (req, res) => {
+  const { originalUrl } = req.body;
+
+  const existingUrl = await UrlModel.findOne({ originalUrl });
+  if (existingUrl) {
+    return res.json({ shortUrl: existingUrl.shortUrl });
+  }
+
+  const shortUrl = shortid.generate();
+
+  const newUrl = new UrlModel({
+    originalUrl,
+    shortUrl,
   });
 
+  await newUrl.save();
 
-  routes.post('/create', async (req, res) => {
-    const { originalUrl } = req.body;
-  
-    // Check if the URL is already shortened
-    const existingUrl = await UrlModel.findOne({ originalUrl });
-    if (existingUrl) {
-      return res.json({ shortUrl: existingUrl.shortUrl }); 
-    }
-  
-    // Generate a new short URL
-    const shortUrl = shortid.generate();
-  
-    // Create a new URL document
-    const newUrl = new UrlModel({
-      originalUrl,
-      shortUrl
-    });
-  
-    // Save the URL document
-    await newUrl.save();
-  
-    res.json({ shortUrl });
-  });
-  
-  // Define the API endpoint for redirecting to the original URL
-  routes.get('/:shortUrl', async (req, res) => {
-    const { shortUrl } = req.params;
-  
-    // Find the URL document with the given short URL
-    const url = await UrlModel.findOneAndUpdate({ shortUrl },{$inc:{count:1}});
+  res.json({ shortUrl });
+});
+
+routes.get("/:shortUrl", async (req, res) => {
+  const { shortUrl } = req.params;
+
+  try {
+    const url = await UrlModel.findOneAndUpdate(
+      { shortUrl },
+      { $inc: { count: 1 } }
+    );
     if (!url) {
-      res.status(404).json({ error: 'URL not found' });
-      return;
+      return res.status(404).json({ error: "URL not found" });
     }
-  
-    res.redirect(url.originalUrl);
-  });
 
-  
-
+    return res.redirect(url.originalUrl);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = routes;
